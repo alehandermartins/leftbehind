2e24eb12aefe0cf7a63d6faf17954cb5c4cb53e1
Alejandro <alehander.marti@gmail.com>
2017-11-15 15:10:11 +0100

    small fix

--- services/http.rb
+++ services/http.rb
@@ -4,6 +4,7 @@ module Services
 		 class << self
 
 		 		def notify uuids, game
+		 			return unless BaseController.settings.onesignal
 	 				players = Repos::Users.player_ids(uuids)
 	 			 	params = {
 				    "app_id" => BaseController.settings.onesignal["app_id"],
@@ -12,16 +13,14 @@ module Services
 				    "include_player_ids" => players
 				  }
 
-				  parsed_url = URI.parse('https://onesignal.com/api/v1/notifications')
+				  parsed_url = URI.parse(BaseController.settings.onesignal["uri"])
 				  req = Net::HTTP::Post.new(parsed_url.path,
 				                              'Content-Type'  => 'application/json;charset=utf-8',
 				                              'Authorization' => "Basic #{BaseController.settings.onesignal["rest_key"]}")
 				  http = Net::HTTP.new(parsed_url.host, parsed_url.port)
 				  http.use_ssl = (parsed_url.scheme == "https")
 				  req.body = params.to_json
-
 				  response = http.request(req)
-				  puts response.body
 		 		end
 		 end
 	end
