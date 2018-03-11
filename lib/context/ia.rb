dd1d77f45292730d58e11e6af875541fba068481
alehandermartins <alehander.marti@gmail.com>
2018-03-11 19:36:25 +0100

    inititial locking

--- lib/context/ia.rb
+++ lib/context/ia.rb
@@ -3,14 +3,12 @@ class IA
     @locations = locations
     @location
     @mark_count = 1
-    @locations.lock '8'
     @locations.mark_random @mark_count
   end
 
   def activate
     @locations.unmark_all
     @location = @locations.mark_random @mark_count
-    @locations.lock_random
     @mark_count += 1 if @mark_count < 3
   end
 
